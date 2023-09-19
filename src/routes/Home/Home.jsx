d47929c96a7da81d4b5895545017e8af40503ca4
nmaraz <nataliamaraz@gmail.com>
2023-09-19 20:55:57 -0300

    Incorporación del Carrusel

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,6 +1,22 @@
 import './Home.css'
 import huellas3 from '../../assets/image/huellas3.png'
-import final from '../../assets/image/final.png'
+import guitarra from '../../assets/image/guitarra.png'
+import abuelo from '../../assets/image/abuelo.png'
+import celular from '../../assets/image/celular.png'
+import llavero from '../../assets/image/llavero.png'
+import libro from '../../assets/image/libro.png'
+import peluche from '../../assets/image/peluche.png'
+import billetera from '../../assets/image/billetera.png'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules'
+import 'swiper/css'
+
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+
+
+
 const Home = () => {
   return (
     <>
@@ -21,7 +37,62 @@ const Home = () => {
             </div>
 
             <div className="showcase-img-home">
-              <img className="img-home" src={huellas3} alt="" />
+
+            <Swiper 
+ modules={[Pagination, Scrollbar, A11y, Autoplay]}
+ spaceBetween={30}
+ slidesPerView={1}
+ 
+ loop={true}
+ 
+ pagination={
+  true
+}
+
+ onSwiper={(swiper) => console.log(swiper)}
+ onSlideChange={() => console.log('slide change')}
+ 
+ autoplay={{
+   delay: 2000,
+   disableOnInteraction: false
+ }}
+
+ className='swiper-container'>
+<SwiperSlide className='slide-item'>
+  <img src={huellas3}/>
+</SwiperSlide>
+<SwiperSlide className='slide-item'>
+  <img src={guitarra}/>
+</SwiperSlide>
+<SwiperSlide className='slide-item'>
+  <img src={abuelo}/>
+</SwiperSlide>
+<SwiperSlide className='slide-item'>
+  <img src={celular}/>
+  
+</SwiperSlide>
+<SwiperSlide className='slide-item'>
+  <img src={llavero}/>
+  
+</SwiperSlide>
+<SwiperSlide className='slide-item'>
+  <img src={libro}/>
+    </SwiperSlide>
+    <SwiperSlide className='slide-item'>
+  <img src={peluche}/>
+  
+</SwiperSlide>
+<SwiperSlide className='slide-item'>
+  <img src={billetera}/>
+  
+</SwiperSlide>
+ </Swiper>
+
+
+
+
+
+           
             </div>
           </div>
         </div>
