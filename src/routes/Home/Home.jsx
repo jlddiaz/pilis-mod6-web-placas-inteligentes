7f89a40e68d88730681449b4209b9d654f19cdcc
nmaraz <nataliamaraz@gmail.com>
2023-09-20 17:47:46 -0300

    Mejora del carrusel y cambio de nombre

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,20 +1,18 @@
 import './Home.css'
-import huellas3 from '../../assets/image/huellas3.png'
-import guitarra from '../../assets/image/guitarra.png'
-import abuelo from '../../assets/image/abuelo.png'
-import celular from '../../assets/image/celular.png'
-import llavero from '../../assets/image/llavero.png'
-import libro from '../../assets/image/libro.png'
-import peluche from '../../assets/image/peluche.png'
-import billetera from '../../assets/image/billetera.png'
+import perrito from '../../assets/image/1.png'
+import guitarra from '../../assets/image/5.png'
+import abuelo from '../../assets/image/8.png'
+import celular from '../../assets/image/6.png'
+import llavero from '../../assets/image/3.png'
+import libro from '../../assets/image/4.png'
+import peluche from '../../assets/image/2.png'
+import billetera from '../../assets/image/7.png'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules'
+import { Navigation, EffectCards, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules'
 import 'swiper/css'
 
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
-
-
 
 
 const Home = () => {
@@ -25,7 +23,7 @@ const Home = () => {
           <div className="container-home">
             <div className="showcase-text-home">
               <h1>
-                <span className="title-one-home">SURE PAWS</span>
+                <span className="title-one-home">CUVER</span>
                 <br />
                 </h1>
                 <span className="title-two-home">
@@ -39,15 +37,15 @@ const Home = () => {
             <div className="showcase-img-home">
 
             <Swiper 
- modules={[Pagination, Scrollbar, A11y, Autoplay]}
+ modules={[EffectCards, Pagination, Scrollbar, A11y, Autoplay]}
  spaceBetween={30}
  slidesPerView={1}
- 
+
  loop={true}
  
- pagination={
-  true
-}
+ pagination={{
+          dynamicBullets: true,
+        }}
 
  onSwiper={(swiper) => console.log(swiper)}
  onSlideChange={() => console.log('slide change')}
@@ -59,7 +57,7 @@ const Home = () => {
 
  className='swiper-container'>
 <SwiperSlide className='slide-item'>
-  <img src={huellas3}/>
+  <img src={perrito}/>
 </SwiperSlide>
 <SwiperSlide className='slide-item'>
   <img src={guitarra}/>
